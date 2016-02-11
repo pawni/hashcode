34b01440a580f90f87699c399ef8b22fe446cf12
Darius Fischer <dariusjfischer@gmail.com>
2016-02-11 19:08:56 +0000

    update outputwriter

--- commands.py
+++ commands.py
@@ -5,13 +5,14 @@ class OutputWriter:
         file = open(filename_out, 'w')
 
     def writeLoad(self, droneId, warehouseId, productId, numProducts):
-        file.write('{}L{}{}{}'.format(droneId, warehouseId, productId, numProducts))
+        file.write('{}L{}{}{}\n'.format(droneId, warehouseId, productId, numProducts))
 
     def writeUnload(self, droneId, warehouseId, productId, numProducts):
-        file.write('{}U{}{}{}'.format(droneId, warehouseId, productId, numProducts))
+        file.write('{}U{}{}{}\n'.format(droneId, warehouseId, productId, numProducts))
 
     def writeDeliver(self, droneId, customerId, productId, numProducts):
-        file.write('{}D{}{}{}'.format(droneId, customerId, productId, numProducts))
+        file.write('{}D{}{}{}\n'.format(droneId, customerId, productId, numProducts))
 
     def writeWait(self, droneId, numTimesteps):
-        file.write('{}W{}'.format(droneId, numTimesteps))
+        file.write('{}W{}\n'.format(droneId, numTimesteps))
+
